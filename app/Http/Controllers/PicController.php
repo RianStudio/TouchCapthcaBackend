7750709c25706ccb2533d5a4f205661ad9b87995
CalvinLee <316708138@qq.com>
2016-01-15 18:18:51 +0800

    add database record

--- app/Http/Controllers/PicController.php
+++ app/Http/Controllers/PicController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Libs\ImageCrop;
 use App\Libs\ImageCom;
+use App\Record;
 
 class PicController extends Controller
 {
@@ -22,24 +23,26 @@ class PicController extends Controller
     function output(Request $request){
 
 
-
         //需要定时删除对应的文件
         //每10s进行一次刷新
 
+        //随机生成本地cooike保存的字符串进行匹配
+        $cookie_str=md5(substr( md5(microtime()),2,10));
         //模拟接收kv
 
         $url='http://tback.localhost:8080/';
         //模拟根据返回相关图片
 
-        //根据参数状态返回对于的图片
+        //随机产生原图路径
         $pathToFile="./asset/pics/".rand(1,8).".jpg";
 
 
-      //随机产生位置
+        //随机产生被裁剪的横坐标和纵坐标
         $rand_x=rand(10,200);
         $rand_y=rand(10,150);
 
 
+        //根据时间随机生成按钮路径
         $crop_img='./pic/afterCrop'.time().'.jpg';
 
         $ic=new ImageCrop($pathToFile,$crop_img);
@@ -48,11 +51,12 @@ class PicController extends Controller
         //$ic->SaveAlpha();将补白变成透明像素保存
         $ic->destory();
 
-       //合成可以提示用户的水印
+        //合成可以提示用户的水印
         $save_path="./pic";
         $noic_file='wa'.time().'.jpg';
         $notice=new ImageCom();
         $water_icon='./asset/pics/water.jpg';
+        //背景路径
         $wa= $notice->img_water_mark($pathToFile,$water_icon,$save_path,$noic_file,6,100,$rand_x,$rand_y);
 
 
@@ -64,7 +68,16 @@ class PicController extends Controller
             'a'=>$url.$wa,
             'x'=>$rand_x,
             'y'=>$rand_y,
+            'c'=>$cookie_str,
+
         ];
+
+        //
+
+        //添加记录
+        Record::AddRecord("","","",$pathToFile,$crop_img,$wa,$rand_x,$rand_y,$cookie_str);
+
+        //能进行跨域调用
         header('Access-Control-Allow-Origin:*');
 
         return response()->json($result);
--- app/Record.php
+++ app/Record.php
@@ -10,9 +10,27 @@ class Record extends Model
 
 
     /**
-     * 进行数据填充
+     * 进行记录的保存
      */
-    public static function AddRecord(){
+    public static function AddRecord($create_verify,$void_verify,$user_id,$pic_original,$pic_background,$pic_button,$point_x,$point_y,$cookie_str){
+        $model=new Record();
+        $model->create_verify=$create_verify;
+        $model->void_verify=$void_verify;
+        $model->user_id=$user_id;
+        $model->pic_original=$pic_original;
+        $model->pic_background=$pic_background;
+        $model->pic_button=$pic_button;
+        $model->point_x=$point_x;
+        $model->point_y=$point_y;
+        $model->cookie_str=$cookie_str;
+        $model->save();
+        return $model->id;
+    }
 
+    /**
+     * 删除记录
+     */
+    public static function DelRecord($id){
+        return DB::table('records')->where('id', $id)->delete();
     }
 }
